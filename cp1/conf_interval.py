e04e14fe6aa7b7c6c01bf9c35d67e10d6c0fbd78
Oleksandra Mednikova <olecuprum@gmail.com>
2023-02-22 15:17:06 +0200

    Added confidence interval for the mean of unknown distribution

--- cp1/conf_interval.py
+++ cp1/conf_interval.py
@@ -3,32 +3,44 @@ import scipy.stats as stats
 
 rng = np.random.default_rng()
 gen_sample = lambda n: [rng.normal(0, 1) for _ in range(n)]
-sample_mean = lambda n, sample: sum(sample) / n
+sample_mean = lambda sample: sum(sample) / len(sample)
 
-def sample_var(n, sample, mean=False):
+def sample_var(sample, mean=False):
+    n = len(sample)
     if mean is False:
-        mean = sample_mean(n, sample)
+        mean = sample_mean(sample)
     sample = np.array(sample) - mean
-    result = sum([el ** 2 for el in sample])
+    result = np.sum(sample ** 2)
 
     return result / n
 
-def mean_conf_interval_unknown_var(sample, n, gamma):
-    mean = sample_mean(n, sample)
-    var_sq = sample_var(n, sample, mean) ** 0.5
 
-    z_gamma = stats.t.ppf(q=(1-gamma / 2), df=n-1)
+def mean_conf_interval_unknown_var(sample, gamma):
+    n = len(sample)
+
+    smpl_mean = sample_mean(sample)
+    var_sq = sample_var(sample, smpl_mean) ** 0.5
+
+    z_gamma = stats.t.ppf(q=1-gamma / 2, df=n-1)
     val = z_gamma * (var_sq ** 0.5) / ((n - 1) ** 0.5)
     
-    return mean - val, mean + val
+    return smpl_mean - val, smpl_mean + val
 
-def conf_interval_unknown_dist():
-    pass
+def mean_conf_interval_unknown_dist(sample, gamma):
+    n = len(sample)
 
-def var_conf_interval(sample, n, gamma):
-    var = sample_var(n, sample)
-    print(var ** 0.5)
-    val = n * var
+    smpl_mean = sample_mean(sample)
+    smpl_var = sample_var(sample, smpl_mean)
+
+    z_gamma = stats.norm.ppf(q=1-gamma / 2)
+    val = z_gamma * (smpl_var ** 0.5) / (n ** 0.5)
+
+    return smpl_mean - val, smpl_mean + val
+
+def var_conf_interval(sample, gamma):
+    n = len(sample)
+    smpl_var = sample_var(sample)
+    val = n * smpl_var
 
     z1 = stats.chi2.ppf(q=1-gamma / 2, df=n-1)
     z2 = stats.chi2.ppf(q=gamma / 2, df=n-1)
--- cp1/main.py
+++ cp1/main.py
@@ -1,6 +1,10 @@
 import conf_interval as ci
+import numpy as np
 
 n, gamma = 1000, 0.01
 sample = ci.gen_sample(n)
-#print(ci.mean_conf_interval_unknown_var(sample, n, gamma))
-print(ci.var_conf_interval(sample, n, gamma))
+
+print('Mean:{}\nVariance:{}\n'.format(np.mean(sample), np.var(sample)))
+print(ci.mean_conf_interval_unknown_var(sample, gamma))
+print(ci.var_conf_interval(sample, gamma))
+print(ci.mean_conf_interval_unknown_dist(sample, gamma))
