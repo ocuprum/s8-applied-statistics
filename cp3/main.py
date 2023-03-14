104ad8a7232691edcaee9e82f4bfa7bfd3f600c1
Oleksandra Mednikova <olecuprum@gmail.com>
2023-03-14 20:54:52 +0200

    Finished CLI

--- cp3/main.py
+++ cp3/main.py
@@ -13,15 +13,18 @@ print('4 - Критерій випадковості\n')
 
 task = int(input('Введіть номер завдання: '))
 
-if task == 1:
-    xlam, ylam = 1, 1.2
-    show.show_homogeneity(N, M, gamma, xlam, ylam)
+while task in [1, 2, 3, 4]:
+    if task == 1:
+        xlam, ylam = 1, 1.2
+        show.show_homogeneity(N, M, gamma, xlam, ylam)
 
-elif task == 2:
-    show.show_independence('spearman', N, gamma)
+    elif task == 2:
+        show.show_independence('spearman', N, gamma)
 
-elif task == 3:
-    show.show_independence('kendall', N, gamma)
+    elif task == 3:
+        show.show_independence('kendall', N, gamma)
 
-elif task == 4: 
-    show.show_randomness(N, gamma)
+    elif task == 4: 
+        show.show_randomness(N, gamma)
+    
+    task = int(input('Введіть номер завдання: '))
